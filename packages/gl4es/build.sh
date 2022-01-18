ea4acac32c55c7bd12f0e0c4d0ea81d131d19cd6
Jia Yuan Lo <jylo06g@gmail.com>
2022-01-18 16:31:10 +0800

    gl4es: bump to latest upstream commit

--- packages/gl4es/build.sh
+++ packages/gl4es/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="OpenGL driver for GLES devices"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@suhan-paradkar"
 TERMUX_PKG_VERSION=1.1.4
-TERMUX_PKG_REVISION=2
-_COMMIT=dab338dc5d16807a071454e6116faf09c37f8a31
+TERMUX_PKG_REVISION=3
+_COMMIT=baf1c7482ee8dd138324a1f670fc04706723ed83
 TERMUX_PKG_SRCURL=https://github.com/ptitSeb/gl4es.git
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_DEPENDS="libx11"
@@ -14,7 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_post_get_source() {
-	git pull
+	git fetch --unshallow
 	git reset --hard ${_COMMIT}
 }
 
@@ -23,7 +23,6 @@ termux_step_pre_configure() {
 	# current highest fps output (on my A30s):
 	# -O2 -flto > -O3 -flto > -O2 > -Os > -Os -flto > -O3 > -Oz > -Oz -flto
 	export CFLAGS="${CFLAGS/-Oz/-O2} -flto"
-	export AR="llvm-ar" # remove this after NDK 23 landed
 }
 
 termux_step_post_make_install() {
